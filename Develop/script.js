70ece966f53897135e145e3ec7b48e296f4c8b51
BradleyK <bradleyjkennett@gmail.com>
2022-05-04 01:32:07 -0400

    prompt and alert section

--- Develop/script.js
+++ Develop/script.js
@@ -14,39 +14,69 @@
 //THEN a password is generated that matches the selected criteria
 //THEN the password is either displayed in an alert or written to the page
 
- 
 
-function generatePassword() {
-  var numeric = ["0","1","2","3","4","5","6","7","8","9"];
-  var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  var characters= ["!","@","#","$","%","^","&","*","(",")","?",":",">","<"];
-  var passwordLengthMin = 8;
-  var passwordLengthMax = 128;
-  var passwordLengthTotal = passwordLengthMin , passwordLengthMax;
-  var password = []
-  
-   // prompt the user (8 characters and no more than 128 characters)
-     passwordLengthTotal = parseInt( prompt ("Select the amount of characters for your new password between 8 and 128.") );
-     if (passwordLengthMin < 8 ) {
-       alert ("please enter a valid numeric value between 8 and 128");
+var passwordNumeric = [0,1,2,3,4,5,6,7,8,9];
+var passwordUppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+var passwordLowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+var passwordCharacters = ['#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','['];
+var passwordLength = []
+
+
+function passwordCriteria() {
+
+  //WHEN prompted for the length of the password
+  //THEN I choose a length of at least 8 characters and no more than 128 characters
+
+    passwordLength = parseInt(prompt("Select the amount of characters for your new password, between 8 and 128"));
+    
+    while(passwordLength < 8 || passwordLength > 128) {
+      alert("Please enter a valid character value between 8 and 128")
+      passwordLength = (prompt("Select the amount of characters for your new password, between 8 and 128"));
     }
-     if (passwordLengthMax > 128 ) {
-       alert ("please enter a valid numeric value between 8 and 128")
-     }
-     // prompt the user (whether or not to include lowercase, uppercase, numeric, and/or special characters)
 
-         for (var i =0; i < passwordLengthTotal; i ++) {
-       password.push( numeric + upperCase + lowerCase + characters [Math.floor(Math.random()* numeric + upperCase + lowerCase + characters.length)]);
-     }
-     password=password.join("");
-console.log (password);
- // 2 prompt the user (whether or not to include lowercase, uppercase, numeric, and/or special characters)
- // 3 validate the data (atleast 1 character type selected)
- // 4 display the generated password
- return password
-}
+    //WHEN asked for character types to include in the password
+    //THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+    
+    var includeNumeric = confirm("Will you include NUMERIC characters in your new password? Click OK");
 
+    var includeUppercase = confirm("Will you include UPPERCASE characters in your new password? Click OK")
+
+    var includeLowercase = confirm("Will you include LOWERCASE characters in your new password? Click OK");
+    
+    var includeCharacters = confirm("Will you include SPECIAL CHARACTERS in your new password? Click OK");
+
+     //THEN my input should be validated and at least one character type should be selected
+    
+        
+    if (!includeNumeric && !includeUppercase && !includeLowercase && !includeCharacters) {
+        alert("Your password MUST have atleast one character selected");
+        includeNumeric = confirm("Will you include NUMERIC characters in your new password? Click OK");
+        includeUppercase = confirm("Will you include UPPERCASE characters in your new password? Click OK");
+        includeLowercase = confirm("Will you include LOWERCASE characters in your new password? Click OK");        
+        includeCharacters = confirm("Will you include SPECIAL CHARACTERS in your new password? Click OK");
+    }
+
+    function characterChoices (includeNumeric , includeUppercase , includeLowercase , includeCharacters) {
+      
+      
+
+       alert (" This password contains " + passwordLength + " characters, " + ( includeNumeric = (" Numbers, ")) + 
+       (includeUppercase = (" Uppercase Letters, ")) + (includeLowercase = (" Lowercase Letters ")) + " and " + (includeCharacters = (" Special Characters")));
+    }
+
+    characterChoices(); 
+    
+       
+      
+  
+
+    
+
+
+return password
+  }
+
+   
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
